29c246bf12997a754cfc205f2649647fdb6bd506
Ruben Dario Duarte Anaya <ruben.duarte.anaya@gmail.com>
2023-11-25 15:45:03 -0500

    guess pc random, extended logic and better graphics

--- Python/warm-up/piedra_papel_tijera.py
+++ Python/warm-up/piedra_papel_tijera.py
@@ -1,18 +1,54 @@
+import random
+
+def border(sentence, symbol):
+    print(symbol*20)
+    print(sentence)
+    print(symbol*20)
+    
+def game_data():
+    print()
+    print("*"*20)
+    print(f"{user_name} : {user_guess}")
+    print(f"computer guess: {computer_guess}")
+    print("*"*20)
+
 user_guess = input("Ingresa papel, piedra o tijera: ").lower()
 user_name = input("Ingresa tu nombre: ")
-computer = "tijera"
+choices = ("piedra", "papel", "tijera")
+computer_guess = random.choice(choices)
+winner_user = (f"{user_name} gana!")
+
+game_data()
+
+#empate
+if user_guess == computer_guess:
+    print()
+    border("Empate","=")
 
 #tijera gana a papel
-if user_guess == "tijera":
-    print("Empate")
+elif user_guess == "tijera":
+    if computer_guess == "papel":
+        print()
+        
+        border(winner_user, "*")
+    else:
+        print()
+        border("gana pc","=")
 
 #piedra gana a tijera
-if user_guess == "piedra":
-    print(f"{user_name} gana!")
-
+elif user_guess == "piedra":
+    if computer_guess == "tijera":
+        print()
+        border(winner_user, "*")
+    else:
+        print()
+        border("gana pc", "=")
 #papel gana a piedra
-if user_guess == "papel":
-    print("Gana el pc")
-
-
+elif user_guess == "papel":
+    if computer_guess == "piedra":
+     print()
+     border(winner_user, "*")
+    else:
+        print()
+        border("gana pc", "=")
 
