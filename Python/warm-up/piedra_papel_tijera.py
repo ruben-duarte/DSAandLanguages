5a4b12b95a5be2331ed81babbccded871a4f66dc
Ruben Dario Duarte Anaya <ruben.duarte.anaya@gmail.com>
2023-11-27 12:41:59 -0500

    add more logic and option to win the one who wins twice

--- Python/warm-up/piedra_papel_tijera.py
+++ Python/warm-up/piedra_papel_tijera.py
@@ -7,48 +7,73 @@ def border(sentence, symbol):
     
 def game_data():
     print()
-    print("*"*20)
-    print(f"{user_name} : {user_guess}")
-    print(f"computer guess: {computer_guess}")
-    print("*"*20)
+    print("="*20)
+    print(f"{user_name} : {user_guess}, number of wins: {user_wins}")
+    print(f"computer guess: {computer_guess}, number of wins: {computer_wins}")
+    print("="*20)
 
-user_guess = input("Ingresa papel, piedra o tijera: ").lower()
-user_name = input("Ingresa tu nombre: ")
+
 choices = ("piedra", "papel", "tijera")
-computer_guess = random.choice(choices)
-winner_user = (f"{user_name} gana!")
+rounds = 1
+user_wins = 0
+computer_wins = 0
 
-game_data()
+while True:
+    print("*"*20)
+    print(f"Round  : {rounds} ")
+    print("*"*20)
+    user_guess = input("Ingresa papel, piedra o tijera: ").lower()
+    user_name = input("Ingresa tu nombre: ")
 
-#empate
-if user_guess == computer_guess:
-    print()
-    border("Empate","=")
+    computer_guess = random.choice(choices)
+    winner_user = (f"{user_name} gana!")
 
-#tijera gana a papel
-elif user_guess == "tijera":
-    if computer_guess == "papel":
-        print()
-        
-        border(winner_user, "*")
+    if user_guess not in choices:
+        print("Ingrese una opción válida. Intente de nuevo")
+        continue   
     else:
-        print()
-        border("gana pc","=")
 
-#piedra gana a tijera
-elif user_guess == "piedra":
-    if computer_guess == "tijera":
-        print()
-        border(winner_user, "*")
-    else:
-        print()
-        border("gana pc", "=")
-#papel gana a piedra
-elif user_guess == "papel":
-    if computer_guess == "piedra":
-     print()
-     border(winner_user, "*")
-    else:
-        print()
-        border("gana pc", "=")
+        game_data()
 
+        #empate
+        if user_guess == computer_guess:
+            print()
+            border("Empate","=")
+
+        #tijera gana a papel
+        elif user_guess == "tijera":
+            if computer_guess == "papel":
+                print()
+                
+                border(winner_user, "*")
+                user_wins += 1
+            else:
+                print()
+                border("gana pc","=")
+                computer_wins += 1
+
+        #piedra gana a tijera
+        elif user_guess == "piedra":
+            if computer_guess == "tijera":
+                print()
+                border(winner_user, "*")
+                user_wins += 1
+            else:
+                print()
+                border("gana pc", "=")
+                computer_wins += 1
+        #papel gana a piedra
+        elif user_guess == "papel":
+            if computer_guess == "piedra":
+                print()
+                border(winner_user, "*")
+                user_wins += 1
+            else:
+                print()
+                border("gana pc", "=")
+                computer_wins += 1
+
+    rounds += 1
+
+    if user_wins == 2 or computer_wins == 2:
+        break
